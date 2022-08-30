db5f888adc09e57923434b5aad4aade1c8d04854
Ahmet Hamdi Yavuz <99591875+ahmettyavzz@users.noreply.github.com>
2022-08-31 00:56:48 +0300

    Delete SpotifyDemoProject directory

--- SpotifyDemoProject/java/SpotifyDemo/src/main/java/com/yavuzahmet/SpotifyDemo/dataAccess/abstracts/GenreDao.java
+++
@@ -1,9 +0,0 @@
-package com.yavuzahmet.SpotifyDemo.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yavuzahmet.SpotifyDemo.entity.concretes.Genre;
-
-public interface GenreDao extends JpaRepository<Genre, Integer>{
-
-}
